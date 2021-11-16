cb093af10b6437b891f2f92d9911b65498406ce1
Andrea <andreanuzzo@hotmail.it>
2021-11-16 18:47:08 +0100

    completed exercise

--- js/script.js
+++ js/script.js
@@ -4,26 +4,7 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 
 
 
-// function palindrome(word){
-    
-//     let array = word.split('');
-//     array.reverse();
-
-//     let testPalindrome = '';
-
-//     for(let i = 0; i < array.length; i++){
-//         testPalindrome += array[i];
-//     }
-    
-//     if(testPalindrome == word){
-//         return true;
-//     }
-//     return false;
-// }
-
-let btnPalindrome = document.getElementById('palindrome');
-btnPalindrome.addEventListener('click', function(){
-    let word = prompt('Iserisci una parola');
+function palindrome(word){
     
     let array = word.split('');
     array.reverse();
@@ -35,11 +16,22 @@ btnPalindrome.addEventListener('click', function(){
     }
     
     if(testPalindrome == word){
-       alert(`La parola "${word}" è palindroma`)
-    } else{
-        alert(`La parola "${word}" non è palindroma`)
+        return true;
     }
+    return false;
+}
 
+let btnPalindrome = document.getElementById('palindrome');
+
+btnPalindrome.addEventListener('click', function(){
+
+    let word = prompt('Iserisci una parola');
+
+    if (palindrome(word) == true){
+        alert(`La parola ${word} è palindroma`)
+    } else{
+        alert(`La parola ${word} non è palindroma`)
+    }
 });
 
 
@@ -49,3 +41,26 @@ btnPalindrome.addEventListener('click', function(){
  L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
  Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
  Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
+
+ function numRandom(min, max) {
+     return Math.floor(Math.random() * (parseInt(max)- parseInt(min) + 1) + parseInt(min))
+ }
+
+ let btnPariDispari = document.getElementById('pariDispari');
+
+ btnPariDispari.addEventListener('click', function(){
+    let PariDispari = prompt('Scegli Pari o Dispari');
+    let numb = parseInt(prompt('Scegli un numero da uno a cinque'));
+    
+    let sum = numb + numRandom(1, 5);
+
+
+    if(sum % 2 == 0 && PariDispari == 'pari'){
+       alert(`Hai vinto ${sum}`);
+    } else if (sum % 2 != 0 && PariDispari == 'dispari'){
+        alert(`Hai vinto ${sum}`);
+    }else{
+        alert(`Hai perso ${sum}`);
+    }
+
+});
